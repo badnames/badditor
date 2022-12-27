22da952464504339688f9ba45b41564503c22a99
Moritz Schalk <e52005233@student.tuwien.ac.at>
2022-12-27 11:56:56 +0100

    Setup and test curses build.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
-#include <iostream>
+#include <curses.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    WINDOW *mainWindow = initscr();
+    cbreak();
+    noecho();
+
+    intrflush(stdscr, FALSE);
+    keypad(stdscr, TRUE);
+
+    int width = 0, height = 0;
+    getmaxyx(mainWindow, height, width);
+
+    clear();
+    // Hello World!
+    mvaddstr(height / 2, width / 2, "Hello World!");
+    refresh();
+
+    while(true) {
+        char input = getch();
+        if (input == 'q') break;
+    }
+
+    endwin();
+
     return 0;
 }
